6fdc1ece112aabbacf17750cf8875a8be87c8c3a
Markus Egger <megger@eps-software.com>
2021-07-29 22:36:28 -0500

    Added Geek theme and switched everything to .NET 5

---
+++ CODE.Framework.Wpf.Theme.Geek/Standard/Notification.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CODE.Framework.Wpf.Controls;
+
+namespace CODE.Framework.Wpf.Theme.Geek.Standard
+{
+    /// <summary>
+    /// Interaction logic for Notification.xaml
+    /// </summary>
+    public partial class Notification : ListBoxSmartDataTemplate
+    {
+        public Notification()
+        {
+            InitializeComponent();
+        }
+    }
+}
