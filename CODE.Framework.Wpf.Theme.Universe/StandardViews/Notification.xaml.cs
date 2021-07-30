072a34680446383f6bc0bdc635c9f7dd8f26b1dc
Markus Egger <megger@eps-software.com>
2021-07-30 00:12:28 -0500

    Added the Universe theme

---
+++ CODE.Framework.Wpf.Theme.Universe/StandardViews/Notification.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace CODE.Framework.Wpf.Theme.Universe.StandardViews
+{
+    /// <summary>
+    /// Interaction logic for Notification.xaml
+    /// </summary>
+    public partial class Notification
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Notification()
+        {
+            InitializeComponent();
+        }
+    }
+}
