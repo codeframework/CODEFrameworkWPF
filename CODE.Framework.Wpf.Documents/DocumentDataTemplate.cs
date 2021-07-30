19a0524db91edd004a374aca2405e430b58dc739
Markus Egger <megger@eps-software.com>
2021-07-30 00:51:19 -0500

    Added the Documents namespace

---
+++ CODE.Framework.Wpf.Documents/DocumentDataTemplate.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Markup;
+
+namespace CODE.Framework.Wpf.Documents
+{
+    /// <summary>
+    /// Data template created for easy use in data bound documents
+    /// </summary>
+    [ContentProperty("DocumentVisualTree")]
+    public class DocumentDataTemplate : DataTemplate
+    {
+        /// <summary>
+        /// Gets or sets the document visual tree.
+        /// </summary>
+        /// <value>The document visual tree.</value>
+        public FrameworkContentElement DocumentVisualTree { get; set; }
+
+        /// <summary>
+        /// Loads the document template.
+        /// </summary>
+        /// <returns>DependencyObject.</returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public DependencyObject LoadDocumentTemplate()
+        {
+            if (DocumentVisualTree == null) return null;
+
+            var root = DocumentVisualTree;
+            var expressions = new List<BindingToSet>();
+            var newInstance = CloneObject(root, null, expressions);
+            newInstance.SetValue(DocEx.ForceRefreshExpressionProperty, expressions);
+
+            var fragment = new DocumentFragment { Content = newInstance as FrameworkContentElement };
+            return fragment;
+        }
+
+        private static DependencyObject CloneObject(DependencyObject objectToClone, object container, List<BindingToSet> expressions)
+        {
+            var cloneType = objectToClone.GetType();
+            if (Activator.CreateInstance(cloneType) is not FrameworkContentElement newInstance) return null;
+
+            if (container != null) AddElementToContainer(container, newInstance);
+
+            // Cloning all regular properties
+            var properties = cloneType.GetProperties();
+            foreach (var property in properties)
+                if (property.CanWrite && property.CanRead)
+                {
+                    var originalValue = property.GetValue(objectToClone, null);
+                    property.SetValue(newInstance, originalValue, null);
+                }
+
+            // Cloning all dependency properties
+            var dependencyPropertyFields = cloneType.GetFields(BindingFlags.Static | BindingFlags.Public);
+            foreach (var dependencyPropertyField in dependencyPropertyFields)
+                if (dependencyPropertyField.FieldType == typeof(DependencyProperty))
+                {
+                    var originalValue = dependencyPropertyField.GetValue(objectToClone) as DependencyProperty;
+                    if (originalValue == null) continue;
+
+                    BindingExpression bindingExpression = null;
+                    var frameworkContentElement = objectToClone as FrameworkContentElement;
+                    if (frameworkContentElement != null) bindingExpression = frameworkContentElement.GetBindingExpression(originalValue);
+                    else
+                    {
+                        var frameworkElement = objectToClone as FrameworkElement;
+                        if (frameworkElement != null) bindingExpression = frameworkElement.GetBindingExpression(originalValue);
+                    }
+                    if (bindingExpression != null)
+                    {
+                        var newBinding = new Binding(bindingExpression.ParentBinding.Path.Path) { Source = bindingExpression.ParentBinding.Source };
+                        if (newBinding.Source == null && bindingExpression.ParentBinding.RelativeSource != null)
+                            newBinding.RelativeSource = new RelativeSource
+                            {
+                                Mode = bindingExpression.ParentBinding.RelativeSource.Mode,
+                                AncestorType = bindingExpression.ParentBinding.RelativeSource.AncestorType,
+                                AncestorLevel = bindingExpression.ParentBinding.RelativeSource.AncestorLevel
+                            };
+                        newBinding.Converter = bindingExpression.ParentBinding.Converter;
+                        newBinding.ConverterParameter = bindingExpression.ParentBinding.ConverterParameter;
+                        if (newBinding.Source == null && !string.IsNullOrEmpty(bindingExpression.ParentBinding.ElementName))
+                            newBinding.ElementName = bindingExpression.ParentBinding.ElementName;
+                        newBinding.Mode = bindingExpression.ParentBinding.Mode;
+
+                        var newDependencyPropertyField = cloneType.GetField(dependencyPropertyField.Name, BindingFlags.Static | BindingFlags.Public);
+                        if (newDependencyPropertyField != null)
+                        {
+                            var newDependencyProperty = newDependencyPropertyField.GetValue(newInstance) as DependencyProperty;
+                            if (newDependencyProperty != null)
+                                expressions.Add(new BindingToSet { Binding = newBinding, Property = newDependencyProperty, DependencyObject = newInstance });
+                        }
+                    }
+                    else
+                        newInstance.SetValue(originalValue, objectToClone.GetValue(originalValue));
+                }
+
+            var children = GetChildElements(objectToClone);
+            foreach (var child in children)
+                CloneObject(child, newInstance, expressions);
+
+            return newInstance;
+        }
+
+        /// <summary>
+        /// Gets the child elements.
+        /// </summary>
+        /// <param name="parent">The parent.</param>
+        /// <returns>IEnumerable{DependencyObject}.</returns>
+        private static IEnumerable<DependencyObject> GetChildElements(DependencyObject parent)
+        {
+            var children = new List<DependencyObject>();
+
+            if (parent is Section section)
+            {
+                children.AddRange(section.Blocks);
+                return children;
+            }
+
+            if (parent is Paragraph paragraph)
+            {
+                children.AddRange(paragraph.Inlines);
+                return children;
+            }
+
+            if (parent is ItemsControl items)
+            {
+                children.AddRange(from object item in items.Items select item as DependencyObject);
+                return children;
+            }
+
+            if (parent is Panel panel)
+            {
+                children.AddRange(from object child in panel.Children select child as DependencyObject);
+                return children;
+            }
+
+            return children;
+        }
+
+        private static void AddElementToContainer(object container, object newInstance)
+        {
+            if (container is Section section && newInstance is Block newInstanceBlock) section.Blocks.Add(newInstanceBlock);
+            else
+            {
+                if (container is Paragraph paragraph && newInstance is Inline newInstanceInline) paragraph.Inlines.Add(newInstanceInline);
+                else
+                {
+                    if (container is ItemsControl items) items.Items.Add(newInstance);
+                    else
+                    {
+                        if (container is Panel panel && newInstance is UIElement uiElement) panel.Children.Add(uiElement);
+                        else
+                        {
+                            if (container is ContentControl content) content.Content = newInstance;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CODE.Framework.Wpf.Documents/HtmlSection.cs
@@ -0,0 +1,149 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace CODE.Framework.Wpf.Documents
+{
+    /// <summary>
+    /// Section class that can be populated from HTML
+    /// </summary>
+    public class HtmlSection : Section
+    {
+        /// <summary>HTML string (fragment with limited HTML support)</summary>
+        public string Html
+        {
+            get => (string)GetValue(HtmlProperty);
+            set => SetValue(HtmlProperty, value);
+        }
+
+        /// <summary>HTML string (fragment with limited HTML support)</summary>
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.Register("Html", typeof(string), typeof(HtmlSection), new UIPropertyMetadata("", RepopulateBlocks));
+
+        /// <summary>Second (optional) HTML string (fragment with limited HTML support)</summary>
+        public string Html2
+        {
+            get => (string)GetValue(Html2Property);
+            set => SetValue(Html2Property, value);
+        }
+
+        /// <summary>HTML string (fragment with limited HTML support)</summary>
+        public static readonly DependencyProperty Html2Property = DependencyProperty.Register("Html2", typeof(string), typeof(HtmlSection), new UIPropertyMetadata("", RepopulateBlocks));
+
+        /// <summary>Style assigned to the HTML2 content</summary>
+        public Style Style2
+        {
+            get => (Style)GetValue(Style2Property);
+            set => SetValue(Style2Property, value);
+        }
+
+        /// <summary>Style assigned to the HTML2 content</summary>
+        public static readonly DependencyProperty Style2Property = DependencyProperty.Register("Style2", typeof(Style), typeof(HtmlSection), new UIPropertyMetadata(null, RepopulateBlocks));
+
+        /// <summary>Block UI positioned at the leading left/top edge of the first paragraph</summary>
+        public BlockUIContainer LeadingBlock
+        {
+            get => (BlockUIContainer)GetValue(LeadingBlockProperty);
+            set => SetValue(LeadingBlockProperty, value);
+        }
+
+        /// <summary>Block UI positioned at the leading left/top edge of the first paragraph</summary>
+        public static readonly DependencyProperty LeadingBlockProperty = DependencyProperty.Register("LeadingBlock", typeof(BlockUIContainer), typeof(HtmlSection), new UIPropertyMetadata(null, RepopulateBlocks));
+
+        /// <summary>Width allocated for the leading block</summary>
+        public double LeadingBlockWidth
+        {
+            get => (double)GetValue(LeadingBlockWidthProperty);
+            set => SetValue(LeadingBlockWidthProperty, value);
+        }
+
+        /// <summary>Width allocated for the leading block</summary>
+        public static readonly DependencyProperty LeadingBlockWidthProperty = DependencyProperty.Register("LeadingBlockWidth", typeof(double), typeof(HtmlSection), new UIPropertyMetadata(100d, RepopulateBlocks));
+
+        /// <summary>Leading HTML string (fragment with limited HTML support)</summary>
+        public string LeadingParagraphHtml
+        {
+            get => (string)GetValue(LeadingParagraphHtmlProperty);
+            set => SetValue(LeadingParagraphHtmlProperty, value);
+        }
+
+        /// <summary>Leading HTML string (fragment with limited HTML support)</summary>
+        public static readonly DependencyProperty LeadingParagraphHtmlProperty = DependencyProperty.Register("LeadingParagraphHtml", typeof(string), typeof(HtmlSection), new UIPropertyMetadata("", RepopulateBlocks));
+
+        /// <summary>Trailing HTML string (fragment with limited HTML support)</summary>
+        public string TrailingParagraphHtml
+        {
+            get => (string)GetValue(TrailingParagraphHtmlProperty);
+            set => SetValue(TrailingParagraphHtmlProperty, value);
+        }
+
+        /// <summary>Trailing HTML string (fragment with limited HTML support)</summary>
+        public static readonly DependencyProperty TrailingParagraphHtmlProperty = DependencyProperty.Register("TrailingParagraphHtml", typeof(string), typeof(HtmlSection), new UIPropertyMetadata("", RepopulateBlocks));
+
+        /// <summary>Defines whether spaces are trimmed off at the start of the paragraph</summary>
+        public bool TrimLeadingParagraphSpaces
+        {
+            get => (bool)GetValue(TrimLeadingParagraphSpacesProperty);
+            set => SetValue(TrimLeadingParagraphSpacesProperty, value);
+        }
+
+        /// <summary>Defines whether spaces are trimmed off at the start of the paragraph</summary>
+        public static readonly DependencyProperty TrimLeadingParagraphSpacesProperty = DependencyProperty.Register("TrimLeadingParagraphSpaces", typeof(bool), typeof(HtmlSection), new UIPropertyMetadata(true, RepopulateBlocks));
+
+        /// <summary>Defines whether spaces are trimmed off at the start of the paragraph</summary>
+        public bool TrimLeadingParagraphTabs
+        {
+            get => (bool)GetValue(TrimLeadingParagraphTabsProperty);
+            set => SetValue(TrimLeadingParagraphTabsProperty, value);
+        }
+
+        /// <summary>Defines whether spaces are trimmed off at the start of the paragraph</summary>
+        public static readonly DependencyProperty TrimLeadingParagraphTabsProperty = DependencyProperty.Register("TrimLeadingParagraphTabs", typeof(bool), typeof(HtmlSection), new UIPropertyMetadata(true, RepopulateBlocks));
+
+        /// <summary>Re-creates the actual paragraph inlines based on the HTML as well as leading and trailing inlines</summary>
+        /// <param name="source">Special Paragraph object</param>
+        /// <param name="e">Event arguments</param>
+        private static void RepopulateBlocks(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            if (source is not HtmlSection section) return;
+
+            section.Blocks.Clear();
+            var blocks = section.Html.ToBlocks(section.LeadingParagraphHtml, section.TrailingParagraphHtml, section.TrimLeadingParagraphSpaces, section.TrimLeadingParagraphTabs).ToList();
+            if (blocks.Count > 0)
+            {
+                section.Blocks.AddRange(blocks);
+
+                if (section.LeadingBlock != null)
+                {
+                    if (section.Blocks.FirstBlock is not Paragraph paragraph)
+                    {
+                        paragraph = new Paragraph();
+                        section.Blocks.Add(paragraph);
+                    }
+
+                    var figure = new Figure
+                    {
+                        HorizontalAnchor = FigureHorizontalAnchor.ColumnLeft,
+                        VerticalAnchor = FigureVerticalAnchor.ParagraphTop,
+                        Width = new FigureLength(section.LeadingBlockWidth),
+                        Margin = new Thickness(0),
+                        Padding = new Thickness(0)
+                    };
+                    figure.Blocks.Add(section.LeadingBlock);
+                    if (paragraph.Inlines.FirstInline != null) paragraph.Inlines.InsertBefore(paragraph.Inlines.FirstInline, figure);
+                    else paragraph.Inlines.Add(figure);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(section.Html2))
+            {
+                var blocks2 = section.Html2.ToBlocks(string.Empty, string.Empty, section.TrimLeadingParagraphSpaces, section.TrimLeadingParagraphTabs).ToList();
+                if (blocks2.Count > 0)
+                    section.Blocks.AddRange(blocks2);
+
+                if (section.Style2 != null)
+                    foreach (var block in blocks2)
+                        block.Style = section.Style2;
+            }
+        }
+    }
+}
