be1f4c18ee641881547b53f344c72c6bc49c555b
Markus Egger <megger@eps-software.com>
2021-07-30 00:17:27 -0500

    Added the Vapor theme

---
+++ CODE.Framework.Wpf.Theme.Vapor/Standard/LargeImage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace CODE.Framework.Wpf.Theme.Vapor.Standard
+{
+    /// <summary>
+    /// Interaction logic for LargeImage.xaml
+    /// </summary>
+    public partial class LargeImage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LargeImage"/> class.
+        /// </summary>
+        public LargeImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CODE.Framework.Wpf.Theme.Vapor/Standard/Notification.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace CODE.Framework.Wpf.Theme.Vapor.Standard
+{
+    /// <summary>
+    /// Interaction logic for Notification.xaml
+    /// </summary>
+    public partial class Notification
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Notification"/> class.
+        /// </summary>
+        public Notification()
+        {
+            InitializeComponent();
+        }
+    }
+}
